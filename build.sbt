53fd1b8337f733181d62a7e77d1a27a0be0326f3
Michael Elufimov <elufimov@gmail.com>
2015-10-02 14:18:01 +0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "ReusableRemoteWebDriver"
+
+version := "0.1"
+
+scalaVersion := "2.11.7"
+
+
+libraryDependencies ++= Seq(
+  "org.seleniumhq.selenium" % "selenium-java" % "2.47.1",
+  "com.github.pathikrit" %% "better-files" % "2.8.1"
+)
---
+++ src/main/scala/com/github/elufimov/reusablerwd/ReusableRWD.scala
@@ -0,0 +1,63 @@
+package com.github.elufimov.reusablerwd
+
+import java.io.{FileInputStream, ObjectInputStream, FileOutputStream, ObjectOutputStream}
+import java.lang.reflect.Field
+import java.net.URL
+
+import better.files.File
+import org.openqa.selenium.Capabilities
+import org.openqa.selenium.remote._
+
+/**
+ * Created by elufimov on 01/10/15.
+ */
+class ReusableRWD(
+  remoteAddress: URL,
+  desiredCapabilities: Capabilities
+) extends RemoteWebDriver(
+  new HttpCommandExecutor(remoteAddress),
+  desiredCapabilities,
+  null
+) {
+  override protected def startSession(desiredCapabilities: Capabilities, requiredCapabilities: Capabilities) {}
+}
+
+object ReusableRWD {
+  def initSession(url: URL, capabilities: DesiredCapabilities): RemoteWebDriver = {
+    File("./session").delete(true)
+    if (!File("./session").exists)
+      File("./session").createDirectory()
+
+    val driver = new RemoteWebDriver(url, capabilities)
+    val returnedCapabilities = driver.getClass.getDeclaredField("capabilities")
+    returnedCapabilities.setAccessible(true)
+
+    File("./session/id") < driver.getSessionId.toString
+    File("./session/url") < url.toString
+
+    val os = new ObjectOutputStream(new FileOutputStream("./session/capabilities"))
+    os.writeObject(returnedCapabilities.get(driver).asInstanceOf[Capabilities])
+    os.close()
+
+    driver
+  }
+
+  def loadSession(): RemoteWebDriver = {
+    val id = File("./session/id").contentAsString
+    val url = new URL(File("./session/url").contentAsString)
+    val is = new ObjectInputStream(new FileInputStream("./session/capabilities"))
+    val capabilities = is.readObject().asInstanceOf[Capabilities]
+    is.close()
+    val driver = new ReusableRWD(url, capabilities)
+
+    val sessionId: Field = driver.getClass.getSuperclass.getDeclaredField("sessionId")
+    sessionId.setAccessible(true)
+    sessionId.set(driver, new SessionId(id))
+
+    val returnedCapabilities = driver.getClass.getSuperclass.getDeclaredField("capabilities")
+    returnedCapabilities.setAccessible(true)
+    returnedCapabilities.set(driver, capabilities)
+
+    driver
+  }
+}
